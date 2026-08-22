c8145d857a79c22dafbd3079f9ea81b57048a800
Terryia <cyp@TerrainkitePC6.localdomain>
2026-08-22 09:40:04 +0000

    Update app and gitignore

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+
 from attendance import attendance_bp
 from leave import leave_bp
+from payroll import payroll_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(attendance_bp, url_prefix='/api/attendance')
 app.register_blueprint(leave_bp, url_prefix='/api/leave')
+app.register_blueprint(payroll_bp, url_prefix='/api/payroll')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
