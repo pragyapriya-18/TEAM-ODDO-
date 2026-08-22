e490707327012bb76145e6792cfbdc33e65de8b8
Terryia <cyp@TerrainkitePC6.localdomain>
2026-08-22 11:03:07 +0000

    i

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 from attendance import attendance_bp
 from leave import leave_bp
 from payroll import payroll_bp
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(attendance_bp, url_prefix='/api/attendance')
 app.register_blueprint(leave_bp, url_prefix='/api/leave')
